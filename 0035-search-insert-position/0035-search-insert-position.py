67a13688715257420326a11ccdb1b4b2c1d5ff46
Marich666 <44069995+Marich666@users.noreply.github.com>
2024-09-24 23:00:29 +0300

    Time: 55 ms (18.45%), Space: 17.3 MB (12.99%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -6,7 +6,7 @@ class Solution:
             mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
+            if nums[mid] < target:
                 left = mid + 1
             else:
                 right = mid - 1
