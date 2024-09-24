1a8d8e332da7572537c5521448ffc91f473a47c4
Marich666 <44069995+Marich666@users.noreply.github.com>
2024-09-25 00:06:15 +0300

    Time: 42 ms (12.2%), Space: 16.6 MB (53.11%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -5,7 +5,7 @@ class Solution:
             x = 1/x
             n = -n
         while n > 0:
-            if (n % 2) == 1:
+            if n % 2 == 1:
                 res *= x
             x *= x
             n //= 2
